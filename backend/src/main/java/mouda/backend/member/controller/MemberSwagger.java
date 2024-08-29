02b0029813cb1f04d132ad4e20dc2c85aaeda3e2
ay-eonii <naayen@naver.com>
2024-08-29 16:30:11 +0900

    docs: 멤버 DTO, 예외 응답 문서화

--- backend/src/main/java/mouda/backend/member/controller/MemberSwagger.java
+++ backend/src/main/java/mouda/backend/member/controller/MemberSwagger.java
@@ -4,11 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import mouda.backend.common.RestResponse;
 import mouda.backend.config.argumentresolver.LoginDarakbangMember;
 import mouda.backend.darakbangmember.domain.DarakbangMember;
+import mouda.backend.exception.ErrorResponse;
 import mouda.backend.member.dto.response.MemberFindResponse;
 
 public interface MemberSwagger {
@@ -16,6 +19,9 @@ public interface MemberSwagger {
 	@Operation(summary = "로그인 된 회원의 정보 조회", description = "로그인 된 회원의 정보를 조회합니다.")
 	@ApiResponses({
 		@ApiResponse(responseCode = "200", description = "조회 성공!"),
+		@ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "401", description = "가입한 다락방이 아닙니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "404", description = "다락방이 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
 	})
 	ResponseEntity<RestResponse<MemberFindResponse>> findMyInfo(
 		@PathVariable Long darakbangId,
--- backend/src/main/java/mouda/backend/member/dto/response/MemberFindResponse.java
+++ backend/src/main/java/mouda/backend/member/dto/response/MemberFindResponse.java
@@ -1,7 +1,13 @@
 package mouda.backend.member.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "멤버 정보", description = "로그인 회원의 정보 조회할 때 사용")
 public record MemberFindResponse(
+	@Schema(description = "닉네임", example = "테니")
 	String nickname,
+
+	@Schema(description = "프로필 사진", example = "profile.png")
 	String profile
 ) {
 }
